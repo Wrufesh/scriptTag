a5c032c3e2819da4eccd6237d6b84307d0a224d3
Rupesh Singh <wrufesh@gmail.com>
2017-08-20 03:04:45 +0545

    disable sales.js added

---
+++ disable_sales.js
@@ -0,0 +1,44 @@
+var loadScript = function (url, callback) {
+
+    /* JavaScript that will load the jQuery library on Google's CDN.
+     We recommend this code: http://snipplr.com/view/18756/loadscript/.
+     Once the jQuery library is loaded, the function passed as argument,
+     callback, will be executed. */
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState) {  //IE
+        script.onreadystatechange = function () {
+            if (script.readyState == "loaded" ||
+                script.readyState == "complete") {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+
+};
+
+var myAppJavaScript = function ($) {
+    /* Your app's JavaScript here.
+     $ in this scope references the jQuery object we'll use.
+     Don't use 'jQuery', or 'jQuery191', here. Use the dollar sign
+     that was passed as argument.*/
+    $('body').append('<p>Your app is using jQuery version ' + $.fn.jquery + '</p>');
+};
+
+if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < 1.7)) {
+    loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function () {
+        jQuery191 = jQuery.noConflict(true);
+        myAppJavaScript(jQuery191);
+    });
+} else {
+    myAppJavaScript(jQuery);
+}
